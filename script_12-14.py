1add9e9131d68cd911d4a052b313e6bb43ecd243
mateuszjan377 <mateusz2001.pl@wp.pl>
2023-04-29 14:47:10 +0200

    dodalismy dwie defincję 2000/1992

--- script_12-14.py
+++ script_12-14.py
@@ -60,7 +60,110 @@ class Transformacje:
         l = atan2(Y , X)
         return(f,l,h)
     
+    def x_y_2000(self, X, Y, Z):
+        '''
+        
+
+        Parameters
+        ----------
+        x : TYPE
+            DESCRIPTION.
+        y : TYPE
+            DESCRIPTION.
+        z : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        x_00 : TYPE
+            DESCRIPTION.
+        y_00 : TYPE
+            DESCRIPTION.
+
+        '''
+        a = self.a
+        e2 = self.e2
+        fi = self.hirvonen(X,Y,Z)[0]
+        lam = self.hirvonen(X,Y,Z)[1]
+        if abs(degrees(lam) - 15) <= 1.5:
+            l0_deg = 15
+        elif abs(degrees(lam) - 18) < 1.5:
+            l0_deg = 18
+        elif abs(degrees(lam) - 21) <= 1.5:
+            l0_deg = 21
+        else:
+            l0_deg = 24
+        l0 = radians(l0_deg)
+        a2 = a**2
+        b2 = a2 * (1 - e2)
+        e_2 = (a2 - b2)/b2
+        dl = lam - l0
+        dl2 = dl**2
+        dl4 = dl**4
+        t = tan(fi)
+        t2 = t**2
+        t4 = t**4
+        n2 = e_2 * (cos(fi)**2)
+        n4 = n2 ** 2
+        N = self.a / sqrt(1 - self.e2 * sin(fi)**2)
+        e4 = e2**2
+        e6 = e2**3
+        A0 = 1 - (e2/4) - ((3*e4)/64) - ((5*e6)/256)
+        A2 = (3/8) * (e2 + e4/4 + (15*e6)/128)
+        A4 = (15/256) * (e4 + (3*e6)/4)
+        A6 = (35*e6)/3072
+        sigma = a * ((A0 * fi) - A2 * sin(2*fi) + A4 * sin(4*fi) - A6 * sin(6*fi))
+        xgk = sigma + ((dl**2)/2) * N * sin(fi) * cos(fi) * (1 + ((dl**2)/12)*(cos(fi)**2)*(5 - t2 + 9 * n2 + 4 * n4) + (dl4/360) * (cos(fi)**4)*(61 - (58 * t2) + t4 + (270 * n2) - (330 * n2 * t2)))
+        ygk = dl * N * cos(fi) * (1 + (dl2/6) * (cos(fi)**2) * (1 - t2 + n2) + (dl4/120) * (cos(fi)**4) * (5 - (18 * t2) + t4 + (14 * n2) - 58 * n2 * t2))
+        strefa = int(l0 * 180/pi)/3
+        x_00 = xgk * 0.999923
+        y_00 = ygk * 0.999923 + strefa * 1000000 + 500000
+        return x_00, y_00
     
+    def x_y_1992(self, X, Y, Z):
+       '''
+        
+
+        Parameters
+        ----------
+        x : TYPE
+            DESCRIPTION.
+        y : TYPE
+            DESCRIPTION.
+        z : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        '''
+        a = self.a
+        e2 = self.e2
+        fi= self.hirvonen(X,Y,Z)[0]
+        lam = self.hirvoen(X,Y,Z)[1]
+        m = 0.9993
+        
+        N = self.a/sqrt(1-self.e2*sin(fi)**2)
+        e2p = self.e2/(1-self.e2)
+        t = tan(fi)
+        n2 = e2p * cos(fi)**2
+        l0 = radians(19)
+        lam1 = lam - l0
+        
+        A0 = 1 - (self.e2/4) - ((3*(self.e2**2))/64) - ((5*(self.e2**3))/256)
+        A2 = (3/8) * (self.e2 + ((self.e2**2)/4) + ((15 * (self.e2**3))/128))
+        A4 = (15/256) * (self.e2**2 + ((3*(self.e2**3))/4))
+        A6 = (35 * (self.e2**3))/3072
+        
+        sig = self.a * ((A0*fi) - (A2*sin(2*fi)) + (A4*sin(4*fi)) - (A6*sin(6*fi)))
+        x = sig + ((lam1**2)/2) * (N*sin(fi)*cos(fi)) * (1 + ((lam1**2)/12) * ((cos(fi))**2) * (5 - t**2 + 9*n2 + 4*(n2**2)) + ((lam1**4)/360) * ((cos(fi))**4) * (61 - (58*(t**2)) + (t**4) + (270*n2) - (330 * n2 *(t**2))))
+        y = lam1 * (N * cos(fi)) * (1 + ((((lam1**2)/6) * (cos(fi))**2) * (1-(t**2) + n2)) +  (((lam1**4)/(120)) * (cos(fi)**4)) * (5 - (18 * (t**2)) + (t**4) + (14*n2) - (58*n2*(t**2))))
+            
+        x1992 = x * m - 5300000
+        y1992 = y * m + 500000
+            
+        return(x1992, y1992)
     
     
     
