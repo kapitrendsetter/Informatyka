b528a3e92307c93f7df9878281839dee7ee78e13
Kacper <kedrakacper56@gmail.com>
2023-04-29 18:36:54 +0200

    biblioteka args

--- script_12-14.py
+++ script_12-14.py
@@ -331,10 +331,41 @@ for A,B,C in zip(N,E,U):
     plik.write(f'{A},   {B},      {C} \n')
 plik.close()
  
+
+
+
+
+
+
+
+   
+from argparse import ArgumentParser 
+
+parser = ArgumentParser()
+parser.add_argument('-m', '--m', type = str, help = 'podaj elipsoide')
+parser.add_argument('-x', '--x', type=float)
+parser.add_argument('-y', '--y', type=float)
+parser.add_argument('-z', '--z', type=float)
+args = parser.parse_args()
+
+
+geo = Transformation(model = args.m)
+f1, l1, h = geo.hirvonen(args.x, args.y, args.z) 
+N1, E, U = geo.XYZ_neu(args.x, args.y, args.y) 
+x_92, y_92 = geo.x_y_1992(args.x, args.y, args.z)
+x_20, y_20 = geo.x_y_2000(args.x, args.y, args.z)
+
+
+
+f = f1 * 180 /pi 
+l = l1 * 180 /pi 
+print(round(f,5),round(l,5), round(h,5))
+print(round(x_20,3), round(y_20,3))
+print(round(x_92,3), round(y_92,3))
+print(round(N1,3), round(E,3), round(U,3))   
+
     
-    
-    
-   # jp2 gmd
+
     
     
     
