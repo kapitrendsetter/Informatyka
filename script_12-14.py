881e06b3db52e14ebf4e21285e9de9ea855881c8
mateuszjan377 <mateusz2001.pl@wp.pl>
2023-04-29 13:21:59 +0200

    dodałem hirvonena

--- script_12-14.py
+++ script_12-14.py
@@ -1,1 +1,67 @@
+from math import *
 
+o = object()
+
+class Transformacje:
+    def __init__(self, model: str = "wgs84"):
+        """
+        Parametry elipsoid:
+            a - duża półoś elipsoidy - promień równikowy
+            b - mała półoś elipsoidy - promień południkowy
+            flat - spłaszczenie
+            ecc2 - mimośród^2
+        + WGS84: https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+        + Inne powierzchnie odniesienia: https://en.wikibooks.org/wiki/PROJ.4#Spheroid
+        + Parametry planet: https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html
+        """
+        if model == "wgs84":
+            self.a = 6378137.0 # semimajor_axis
+            self.b = 6356752.31424518 # semiminor_axis
+        elif model == "grs80":
+            self.a = 6378137.0
+            self.b = 6356752.31414036
+        elif model == "mars":
+            self.a = 3396900.0
+            self.b = 3376097.80585952
+        else:
+            raise NotImplementedError(f"{model} model not implemented")
+        self.flat = (self.a - self.b) / self.a
+        self.ecc = sqrt(2 * self.flat - self.flat ** 2) # eccentricity  WGS84:0.0818191910428 
+        self.ecc2 = (2 * self.flat - self.flat ** 2) # eccentricity**2
+
+    def hirvonen(self,X,Y,Z):
+        '''
+        
+
+        Parameters
+        ----------
+        X : TYPE
+            DESCRIPTION.
+        Y : TYPE
+            DESCRIPTION.
+        Z : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        '''
+        p = sqrt(X**2 + Y**2)
+        f = atan(Z / (p * (1 - self.e2)))
+        while True:
+            N = self.a / sqrt(1 - self.e2 * sin(f)**2)
+            h = (p/cos(f)) - N
+            fp = f
+            f = atan(Z/(p*(1 - self.e2 * N/ (N +h))))
+            if abs(fp - f) < (0.000001/206265):
+                break
+        l = atan2(Y , X)
+        return(f,l,h)
+    
+    
+    
+    
+
+    
+    
